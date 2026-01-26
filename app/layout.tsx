1f27ea2dc1f4bd48293dafb80960583d0d07a8e8
Noah Gallego <noah@helloml.app>
2026-01-26 11:39:55 -0800

    Fix Google Maps API async loading warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,8 +35,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}&libraries=places`}
-          strategy="beforeInteractive"
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}&libraries=places&loading=async`}
+          strategy="afterInteractive"
+          async
         />
       </head>
       <body
